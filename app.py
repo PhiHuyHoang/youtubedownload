ba81b56f590c9604ebf35e548fe7de1cf457e37d
Phi <hoang.phi@sap.com>
2018-06-26 17:40:06 +0200

    commit

--- app.py
+++ app.py
@@ -2,19 +2,13 @@ import json, requests, sys
 import json
 import os
 import youtube_dl
-from flask import Flask, render_template, json, request
+from flask import Flask, render_template, json, request, send_file
 from flask import request
-from flask import make_response
-from random import choice, randint
 import urllib
 from bs4 import BeautifulSoup
 # Flask app should start in global layout
 app = Flask(__name__)
-
-link = []
-title = []
-display = {}
-
+import re
 def make_savepath(title):
     return os.path.join("%s.mp3" % (title))
 
@@ -23,35 +17,50 @@ def main():
     return render_template('index.html')
 
 
-@app.route('/webhook', methods=['POST','GET'])
+@app.route('/webhook', methods=['POST'])
 def webhook():
 	search = request.form['lname']
-	query_string = urllib.parse.urlencode({"search_query" : search})
+	query_string = urllib.parse.urlencode({"search_query": search})
 	page = requests.get("http://www.youtube.com/results?" + query_string)
 	soup = BeautifulSoup(page.content, 'html.parser')
-	for vid in soup.findAll(attrs={'class':'yt-uix-tile-link'}):
-		link.append('https://www.youtube.com' + vid['href'])
-		title.append(vid['title'])
-		display[vid['title']] = 'https://www.youtube.com' + vid['href']
+	display = {}
+	for vid in soup.find_all(class_="yt-lockup-content"):
+		print(vid.find("a")["href"])
+		print(vid.find("a")["title"])
+		print(vid.find(class_="yt-lockup-description yt-ui-ellipsis yt-ui-ellipsis-2"))
+		if vid.find(class_="yt-lockup-description yt-ui-ellipsis yt-ui-ellipsis-2") is not None:
+			des = vid.find(class_="yt-lockup-description yt-ui-ellipsis yt-ui-ellipsis-2").get_text()
+		else:
+			des = " "
+		link = "https://youtube.com"+vid.find("a")["href"]
+		thumbnail = "http://img.youtube.com/vi/%s/0.jpg" % vid.find("a")["href"][9:]
+		display[vid.find("a")["title"]] = [thumbnail,des,link]
 	return render_template('result.html', display = display )
 
-@app.route('/download', methods=['POST','GET'])
+@app.route('/download', methods=['POST'])
 def download():
+	url = request.form["song"]
 	options = {
-	    'format': 'bestaudio/best', # choice of quality
-	    'extractaudio' : True,      # only keep the audio
-	    'audioformat' : "mp3",      # convert to mp3
-	    'outtmpl': '%(id)s',        # name the file the ID of the video
-	    'noplaylist' : True,}       # only download single song, not playlist
+		'format': 'bestaudio/best',  # choice of quality
+		'extractaudio': True,  # only keep the audio
+		'audioformat': "mp3",  # convert to mp3
+		'outtmpl': '%(id)s',  # name the file the ID of the video
+		'noplaylist': True, }  # only download single song, not playlist
 
 	ydl = youtube_dl.YoutubeDL(options)
-	i = int(request.form['lname'])
-	print(link[i],':','Chosen link')
-	savepath = make_savepath(title[i])
+	song_name = ydl.extract_info(url, download=False)
+	savepath = make_savepath(song_name['title'].replace(" ", ""))
+	savepath = re.sub('[^A-Za-z0-9]+', '', savepath)
+	savepath = make_savepath((savepath))
 	with ydl:
-	    result = ydl.extract_info(link[int(input())], download=True)
-	    os.rename(result['id'], savepath)
-	    print("Downloaded and converted %s successfully!" % savepath)
+		result = ydl.extract_info(url, download=True)
+		os.rename(result['id'], savepath)
+		print("Downloaded and converted %s successfully!" % savepath)
+		try:
+			return send_file(savepath, attachment_filename=savepath, as_attachment=True)
+		except Exception as e:
+			return str(e)
+
 
 if __name__ == '__main__':
     app.run()
