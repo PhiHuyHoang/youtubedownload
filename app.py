1c8f76efd31f802ab1527e105334f1ac980d1764
Phi Huy Hoang <luumanhlapdi@gmail.com>
2018-06-27 22:34:02 +0200

    update

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import json, requests, sys
 import os
 import youtube_dl
-from flask import Flask, render_template, json, request, send_from_directory,request
+from flask import Flask, render_template, json, request, send_file,request
 import urllib
 from bs4 import BeautifulSoup
 import datetime
@@ -61,7 +61,7 @@ def download():
 		os.rename(result['id'], savepath)
 		print("Downloaded and converted %s successfully!" % savepath)
 		try:
-			return send_from_directory(savepath,savepath)#, as_attachment=True)
+			return send_file(savepath,savepath, as_attachment=True)
 		except Exception as e:
 			return str(e)
 
