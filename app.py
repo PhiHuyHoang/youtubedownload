c20d001fdd96c150486fbaeeea785ba20c1da027
Phi Huy Hoang <luumanhlapdi@gmail.com>
2018-06-29 14:35:49 +0200

    update

--- app.py
+++ app.py
@@ -17,11 +17,13 @@ def make_savepath(title):
 
 @app.route('/', methods=['GET'])
 def main():
+	search = request.form['search']
 	URL="http://suggestqueries.google.com/complete/search?client=firefox&q=Hel Wor"
 	headers = {'User-agent':'Mozilla/5.0'}
 	response = requests.get(URL, headers=headers)
 	result = json.loads(response.content.decode('utf-8'))
 	suggest = result[1]
+	print(suggest)
 	return render_template('index.html',year = year,suggest = suggest)
 
 
