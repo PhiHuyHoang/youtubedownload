8679550e939ce66dcde892e4ce619823276be7fb
Phi Huy Hoang <luumanhlapdi@gmail.com>
2018-06-29 14:45:00 +0200

    update

---
+++ static/js/autocomplete.js
@@ -0,0 +1,8 @@
+﻿$(function(){
+var games = ["Baseball","Tennis","Golf","Cricket","Football","Hockey","Badminton","Volleyball","Boxing","Kabaddi","Chess","Long Jump","High Jump","Racing","Handball","Swimming","Wrestling"];
+
+$("#autocomplete").autocomplete({
+source: games
+});
+
+});
